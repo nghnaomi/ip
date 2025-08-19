20d6da293175bb453e949e2366b8bca21dc11ee2
Naomi Ng <nghnaomi04@gmail.com>
2025-08-19 17:08:20 +0800

    level 0

---
+++ src/main/java/Alune.java
@@ -0,0 +1,14 @@
+public class Alune {
+    public static void main(String[] args) {
+        String logo = 
+                  "       _                  \n"
+                + "  __ _| |_   _ _ __   ___ \n"
+                + " / _` | | | | | '_ \\ / _ \\\n"
+                + "| (_| | | |_| | | | |  __/\n"
+                + " \\__,_|_|\\__,_|_| |_|\\___|\n";
+        
+        System.out.println(logo + "\nhi i'm alune.\nwhat can i do for you?" + 
+                                    "\n\n«────────── « ⋅ʚ♡ɞ⋅ » ──────────»" +
+                                    "\n\nbye, see you again.");
+    }
+}
